2151ee61203397788927a9ea3f189cecdada2597
0beron <robin.fairey@gmail.com>
2025-03-31 20:11:44 +0100

    More base path shenanigans

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	// This is where the site sets its global CSS, and any elements that appear on all pages
 	import '../app.css';
+        import { base } from '$app/paths';
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
@@ -9,5 +10,9 @@
 	// If you want a custom font from fontsource, import it here, eg
 	// import '@fontsource/my-custom-font/400.css';  // use My Custom Font at weight 400
 </script>
+
+<svelte:head>
+	<base href="{base}" />
+</svelte:head>
 
 {@render children?.()}
